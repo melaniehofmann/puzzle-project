f012b2074621de6f43e7cdcb86d6915897c67a49
Melanie Hofmann <melaniehofmann@gmx.at>
2019-11-21 20:38:43 +0100

    Init

---
+++ puzzle-project/src/main/java/com/hofmann/puzzle/PuzzleInputException.java
@@ -0,0 +1,12 @@
+/*
+ * Copyright, 2019, DI Hofmann Melanie - All Rights Reserved.
+ */
+package com.hofmann.puzzle;
+
+@SuppressWarnings("serial")
+public class PuzzleInputException extends RuntimeException {
+
+	public PuzzleInputException(final String message) {
+		super(message);
+	}
+}
